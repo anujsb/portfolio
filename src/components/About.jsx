66e7d2227701f29971ed27b2c5bf01679f7a6ed4
anujsb <anujsbhuyar@gmail.com>
2023-08-19 01:37:35 +0530

    trial1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const ServiceCard = ({ index, title, icon }) => (
   // <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className=' content-center p-[1px] rounded-[20px] shadow-card flex-[1]'
+      className=' content-center p-[1px] rounded-[20px] shadow-card flex-[0.75]'
     >
       <div
         options={{
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9XiLVDrZS-Yhf0WRYN7wxnSwT3hL5FCY",
+  authDomain: "anuj-bhuyar.firebaseapp.com",
+  projectId: "anuj-bhuyar",
+  storageBucket: "anuj-bhuyar.appspot.com",
+  messagingSenderId: "433204603003",
+  appId: "1:433204603003:web:bd5ee0f9b08960b6ec5ade",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
