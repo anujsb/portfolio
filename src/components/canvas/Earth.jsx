c4bbe42a8fb0c102c14bb6e4169a25c72f8d7e59
anujsb <anujsbhuyar@gmail.com>
2023-08-21 14:50:45 +0530

    removed earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,14 +4,6 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-// const Earth = () => {
-//   const earth = useGLTF("./planet/scene.gltf");
-
-//   return (
-//     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
-//   );
-// };
-
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
