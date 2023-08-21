92bdc3b49ec68556637761a56fafacffa5023235
anujsb <anujsbhuyar@gmail.com>
2023-08-21 14:50:09 +0530

    removed earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -16,39 +16,37 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <group scale={2.5} position-y={0} rotation-y={0}>
-      <primitive object={earth.scene} />
-    </group>
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
 const EarthCanvas = () => {
   return (
+    <div>.</div>
+    // <Canvas
+    //   shadows
+    //   frameloop='demand'
+    //   dpr={[1, 2]}
+    //   gl={{ preserveDrawingBuffer: true }}
+    //   camera={{
+    //     fov: 45,
+    //     near: 0.1,
+    //     far: 200,
+    //     position: [-4, 3, 6],
+    //   }}
+    // >
+    //   <Suspense fallback={<CanvasLoader />}>
+    //     <OrbitControls
+    //       autoRotate
+    //       enableZoom={false}
+    //       maxPolarAngle={Math.PI / 2}
+    //       minPolarAngle={Math.PI / 2}
+    //     />
+    //     <Earth />
 
-    <Canvas
-      shadows
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
-
-        <Preload all />
-      </Suspense>
-    </Canvas>
+    //     <Preload all />
+    //   </Suspense>
+    // </Canvas>
   );
 };
 
